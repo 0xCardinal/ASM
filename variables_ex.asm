7236b52f495e0054b7f1dc7b5e87c77c1b2a6734
Kumar Ashwin <krashwin00@gmail.com>
2020-06-03 06:54:41 +0530

    Adds code related to variables

---
+++ variables_ex.asm
@@ -0,0 +1,40 @@
+; Author: Kumar Ashwin
+; Variables
+
+global _start
+
+_start:
+    
+    ; About data types consult Notes.md
+
+    ; we are skipping ebx because it contains file_descriptor
+    mov eax, 0x4
+    mov ecx, definebyte1
+    mov edx, byte1L
+    int 0x80
+
+    mov eax, 0x4
+    mov ecx, definebyte2
+    mov edx, byte2L
+    int 0x80
+
+    mov eax, 0x4
+    mov ecx, defineword
+    mov edx, wordL
+    int 0x80
+
+
+    ; Exit w/ return code 0
+    mov eax, 0x1
+    mov ebx, 0x0
+    int 0x80
+
+    definebyte1: db "Assembly", 0x0a
+    byte1L equ $-definebyte1    
+
+    definebyte2: db "Language", 0x0a
+    byte2L equ $-definebyte2
+
+    defineword: dw "Too much writing", 0x0a
+    wordL equ $-defineword
+
